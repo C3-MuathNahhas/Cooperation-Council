eb7efcf4b047ad5f69898c9b349744681474e3b9
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-08 23:14:36 +0300

    finish ticket createHotelBooking

--- backend/routers/controllers/HotelBooking/hotelBooking.js
+++ backend/routers/controllers/HotelBooking/hotelBooking.js
@@ -3,18 +3,25 @@ const hotelBookingModel = require("../../../db/models/hotelBookingSchema");
 const createHotelBooking = () => {
   const { location, capacity, rooms } = req.body;
 
-  const newHotelBookig=new hotelBookingModel({
-
+  const newHotelBookig = new hotelBookingModel({
     location,
-     capacity,
-      rooms 
-  })
-  newHotelBookig.save().
-  then((result)=>{
-      result.status(201).json({
-          succes:true,
-          success: 'new hotel booking created'
-      })
-  })
-  .catch((err)=>{})
+    capacity,
+    rooms,
+  });
+  newHotelBookig
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        succes: true,
+        success: "new hotel booking created",
+        result: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        succes: false,
+        success: "no availability",
+        massage: err,
+      });
+    });
 };
