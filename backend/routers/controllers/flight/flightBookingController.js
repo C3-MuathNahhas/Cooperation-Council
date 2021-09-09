27bdacba63e0e806bf1bb246698ad22f107059a9
Mohammad-ghazal <moh.g.ghazal@gmail.com>
2021-09-09 23:01:06 +0300

    get flights booking by user id

---
+++ backend/routers/controllers/flight/flightBookingController.js
@@ -0,0 +1,37 @@
+const flightBookingModle = require("../../../db/models/FlightBookigSchema");
+const getFlightsBookingByUserId = (req, res) => {
+    const { userId } = req.body.params;
+    flightBookingModle.find({ userId })
+        .populate("User").populate("Flight")
+        .exec()
+        .then((result) => {
+            if (!result) {
+                return res.status(404).json({
+                    success: false,
+                    message: `ther is No flight booking for the user`,
+                });
+            } else {
+
+                return res.status(200).json(
+                    {
+                        success: true,
+                        message: `this is all flights booking for the user`,
+                        flights: result
+
+                    }
+
+                )
+            }
+        })
+};
+
+
+const creatFlightBooking = () => { }
+
+
+
+
+module.exports = {
+    getFlightsBookingByUserId,
+    creatFlightBooking,
+};
