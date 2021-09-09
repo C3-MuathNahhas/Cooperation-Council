630bafc323890989fc834f310953346884ca950f
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-09 13:30:38 -0700

    create flight schema

---
+++ backend/routers/routes/flight.js
@@ -0,0 +1,14 @@
+const express = require("express");
+// const {
+// //   createNewFlight,
+// //   getFlights,
+// } = require("../controllers/flight/flightController");
+
+const { createNewFlight,getFlights}= require("../controllers/flight/flightController");
+const flightRouter = express.Router();
+
+flightRouter.post("/",createNewFlight)
+
+ flightRouter.get("/search",getFlights)
+
+module.exports = flightRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db/db');
+const flightRouter=require("./routers/routes/flight")
 const signUpRouter=require("./routers/routes/auth/signUp")
 const loginRouter=require("./routers/routes/auth/login")
-
+///const flightRouter=require("./routers/routes/flight")
 const app = express();
 const hotelBookingRouter=require("./routers/routes/hotelBooking");
 const hotelRouter=require("./routers/routes/hotle");
@@ -21,6 +22,7 @@ app.use("/hotel",hotelRouter);
 //app routers
 app.use("/signUp",signUpRouter)
 app.use("/login",loginRouter)
+//app.use("/flights",flightRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
