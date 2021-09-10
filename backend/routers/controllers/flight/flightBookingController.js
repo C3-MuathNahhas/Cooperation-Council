8482d01e0bbd61355fb816dd9f64f747e8b4a0da
Mohammad-ghazal <moh.g.ghazal@gmail.com>
2021-09-10 04:02:58 +0300

    getAllFlightsBooking function

--- backend/routers/controllers/flight/flightBookingController.js
+++ backend/routers/controllers/flight/flightBookingController.js
@@ -9,14 +9,14 @@ const getFlightsBookingByUserId = (req, res) => {
             if (!result) {
                 return res.status(404).json({
                     success: false,
-                    message: `ther is No flight booking for the user`,
+                    message: `there is No flight booking for the user`,
                 });
             } else {
 
                 return res.status(200).json(
                     {
                         success: true,
-                        message: `this is all flights booking for the user`,
+                        message: `all flights bookings for the user`,
                         flights: result
 
                     }
@@ -46,7 +46,30 @@ const creatFlightBooking = (req, res) => {
         });
 };
 
+const getAllFlightsBooking = (req, res) => {
+    flightBookingModle.find({})
+        .populate("User").populate("Flight")
+        .exec()
+        .then((result) => {
+            if (!result) {
+                return res.status(404).json({
+                    success: false,
+                    message: `there is no flight booking for any user yet`,
+                });
+            } else {
 
+                return res.status(200).json(
+                    {
+                        success: true,
+                        message: `all flights bookings for all users`,
+                        flights: result
+
+                    }
+
+                )
+            }
+        })
+};
 
 
 
