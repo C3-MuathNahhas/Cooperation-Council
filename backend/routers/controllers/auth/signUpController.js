3522955a75aa89dd226e85c2c4540ab748d347c2
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-10 09:45:27 -0700

    signup

--- backend/routers/controllers/auth/signUpController.js
+++ backend/routers/controllers/auth/signUpController.js
@@ -23,5 +23,17 @@ const createNewUser = (req, res) => {
     });
 };
 
+const getUsers = (req, res) => {
+  signModel
+    .find({})
+    .then((result) => {
+      res.status(201)
+      res.json({ success: true, massage: "getAllUser", result: result })
+    })
+    .catch((err) => {
+      res.status(500)
+      res.json({ success: false, massage: "nothing User" })
+    });
+};
 
 module.exports = { createNewUser ,getUsers};
