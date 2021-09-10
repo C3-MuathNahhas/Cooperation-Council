2d432d8dce9aa973df3bb28476cd9942f61b3a6e
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-10 19:49:48 +0300

    hello

--- backend/routers/routes/flightRoutes/flightBooking.js
+++ backend/routers/routes/flightRoutes/flightBooking.js
@@ -3,20 +3,27 @@ const flightBookingRouter = express.Router();
 
 const {
   getFlightsBookingByUserId,
-  creatFlightBooking, deleteFlightBooking, getAllFlightsBooking,
-  updateFlightBooking, isBookingExist, isFlightFit
+  creatFlightBooking,
+  deleteFlightBooking,
+  getAllFlightsBooking,
+  updateFlightBooking,
+  isBookingExist,
+  isFlightFit,
 } = require("../../controllers/flight/flightBookingController");
-const { updateFlightCapacity } = require("../../controllers/flight/flightController")
-
+const {
+  updateFlightCapacity,
+} = require("../../controllers/flight/flightController");
 
 flightBookingRouter.post("/", creatFlightBooking);
 flightBookingRouter.delete("/:bookingId", deleteFlightBooking);
 flightBookingRouter.get("/allBooking/:userId", getFlightsBookingByUserId);
 flightBookingRouter.get("/allBooking/", getAllFlightsBooking);
-flightBookingRouter.put("/:bookingId", isBookingExist, isFlightFit, updateFlightCapacity, updateFlightBooking);
-
-
-
-
+flightBookingRouter.put(
+  "/:bookingId",
+  isBookingExist,
+  isFlightFit,
+  updateFlightCapacity,
+  updateFlightBooking
+);
 
 module.exports = flightBookingRouter;
