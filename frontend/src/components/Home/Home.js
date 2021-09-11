bb6e309cb9df9b1a7cfc2ea8c351c0cd1beecca8
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-12 01:25:42 +0300

    add new input date

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,23 +1,28 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export const Home = () => {
-    const [flight,setflight]=useState()
-    axios.get("http://localhost:5000/flights/search").then((result)=>{
-        setflight(result)
-         })
+  const [flight, setflight] = useState();
+  axios.get("http://localhost:5000/flights/search").then((result) => {
+    console.log(result);
+    setflight(result);
+  });
   return (
     <div>
       <select>
-      {flight&&flight.map((element)=>{
-          return(
-              <div>
-                  
+        {flight &&
+          flight.map((element, index) => {
+            return (
+              <div key={index}>
+                <option>flight.from</option>
+                <option>flight.to</option>
               </div>
-          )
-      })}
-        
+            );
+          })}
       </select>
+      <div>
+        <input type="date" value="date from" />
+      </div>
     </div>
   );
 };
