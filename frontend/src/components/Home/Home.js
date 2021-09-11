83fd07a3da9db82ce06f3f66064fe72ef41b1d25
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-12 02:46:04 +0300

    work in input

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -3,52 +3,56 @@ import axios from "axios";
 
 export const Home = () => {
   const [flight, setflight] = useState();
+  const [dis, setdis] = useState();
+  const [orig, setorig] = useState();
+  const [dat, setdat] = useState();
 
-  var options = {
-    method: "GET",
-    url: "https://travel-advisor.p.rapidapi.com/hotels/list",
-    params: {
-      location_id: "293919",
-      adults: "1",
-      rooms: "1",
-      nights: "2",
-      offset: "0",
-      currency: "USD",
-      order: "asc",
-      limit: "30",
-      sort: "recommended",
-      lang: "en_US",
-    },
-    headers: {
-      "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-      "x-rapidapi-key": "8445521e28mshff86c1ecb0006e7p1c6694jsnf4eefd40292a",
-    },
+  axios.get("http//:localhost:5000/flights/search", {});
+  const dist = (e) => {
+    setdis(e.target.value);
   };
-
-  axios
-    .request(options)
-    .then(function (response) {
-      setflight(response.data.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  const origo = (r) => {
+    setorig(r.target.value);
+  };
+  const dd = (f) => {
+    setdat(f.target.value);
+  };
+  const click = () => {
+    axios
+      .post("http//:localhost:5000/flights/search", { dis, orig, dat })
+      .then((result) => {
+        setflight(result);
+      });
+  };
   return (
     <div>
-      
-      <select>
-        {flight &&
-          flight.map((element, index) => {
-            return (
-              
-                <option>{element.name}</option>
-              
-            );
-          })}
-      </select><br/><br/>
+      {flight &&
+        flight.map((element, index) => {
+          return (
+            <div key={index}>
+              <h1>{element.flight_name}</h1>
+            </div>
+          );
+        })}
 
-      <input type="date" /><br/><br/>
-      <input type="date" /><br/><br/>
+      <input type="text" onChange={dist} />
+      <br />
+      <br />
+      <input type="text" onChange={origo} />
+      <br />
+      <br />
+      <input type="date" onChange={dd} />
+      <br />
+      <br />
+      <input type="button" onClick={click} value="click" />
+      <br />
+      <br />
+      <input type="date" />
+      <br />
+      <br />
+      <input type="date" />
+      <br />
+      <br />
     </div>
   );
 };
