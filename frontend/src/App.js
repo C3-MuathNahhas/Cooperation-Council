b22bb178721890f9f0e359b7a9de36346e34560e
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-12 03:41:43 -0700

    creatsignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React,{useState,createContext} from 'react';
+import { Route,useRouteMatch} from 'react-router-dom';
+import Login from './components/auth/Login';
+import SignUp from "./components/auth/SignUp";
+
+export const userContext=createContext();
 
 const App = () => {
-	return <div className="App">App component</div>;
-};
+	let {path,url}=useRouteMatch()
+	const[token,setToken]=useState()
+	const state={token,setToken}
+	return (
+	<div className="App">
+		
+    <userContext.Provider value={state}>
+	<Route path="/signup"component={SignUp} />
+	<Route path= {`${path}/login`}component={Login}/>
+	</userContext.Provider>
+
+	
+	
+
+	</div>
+	)
+}
+
 
 export default App;
