aae0026cea4ad0c94f27df2888cb6db799870630
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-12 20:58:10 +0300

    delete test fille

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import React,{useState,createContext} from "react";
+import React, { useState, createContext } from "react";
 import { Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import Table from "./components/ui/Table";
-import Test from "./components/test";
+
 export const UserContext = createContext();
 const App = () => {
-	const[value,setvalue]=useState();
+  const [value, setvalue] = useState();
   return (
     <div className="App">
       <Route path="/home" render={() => <Home setvalue={setvalue} />} />
-	  <UserContext.Provider value={value}>
-      <Table></Table>
-	  </UserContext.Provider >
-	  {/* <Test ></Test> */}
+      <UserContext.Provider value={value}>
+        <Table></Table>
+      </UserContext.Provider>
     </div>
   );
 };
