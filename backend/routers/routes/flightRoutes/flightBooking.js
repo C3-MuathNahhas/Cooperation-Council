da323a4d753c4bd57f394477bcfee0dec53ea3c2
Mohammad ghazal <moh.g.ghazal@gmail.com>
2021-09-12 11:32:51 +0300

    add auth for booking delete

--- backend/routers/routes/flightRoutes/flightBooking.js
+++ backend/routers/routes/flightRoutes/flightBooking.js
@@ -17,7 +17,7 @@ const {
 } = require("../../controllers/flight/flightController");
 
 flightBookingRouter.post("/",Authentication,isFlightFit,updateFlightCapacity, creatFlightBooking);
-flightBookingRouter.delete("/:bookingId", isBookingExist, updateFlightCapacity, deleteFlightBooking);
+flightBookingRouter.delete("/:bookingId",Authentication, isBookingExist, updateFlightCapacity, deleteFlightBooking);
 flightBookingRouter.get("/allBooking/:userId", getFlightsBookingByUserId);
 flightBookingRouter.get("/allBooking/", getAllFlightsBooking);
 flightBookingRouter.put("/:bookingId",Authentication, isBookingExist, isFlightFit, updateFlightCapacity, updateFlightBooking);
