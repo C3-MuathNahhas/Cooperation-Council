454b939f8890cf12691aaf9a2abb418866415278
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-12 05:14:22 -0700

    create component Navigation

---
+++ frontend/src/components/navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React,{useContext} from "react";
+import { Link,useRouteMatch } from "react-router-dom";
+import SignUp from "../auth/SignUp";
+
+import { userContext } from "../../App";
+const Navigation=()=>{
+    let {path,url}=useRouteMatch()
+    const user=useContext(userContext)
+    return(
+      <div>
+    
+      </div>
+    )
+}
+export default Navigation
