4a1a21532a9145ab8273a82bea3f6692a013f271
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-13 03:07:06 -0700

    add heade and img

--- frontend/src/components/contact/Contact.js
+++ frontend/src/components/contact/Contact.js
@@ -4,7 +4,12 @@ const Contact = () => {
   return (
     <div className="mainContact">
       <div className="contact">
-      
+      <h1>Contact Us</h1>
+        <div className="img_1">
+          <img src="https://media.istockphoto.com/photos/flight-attendant-showing-the-emergency-exit-in-an-airplane-picture-id1028463082?k=20&m=1028463082&s=612x612&w=0&h=U-aNIdv9GhclASjewqlXSfTaELmq0nIUyjJ6APR4IR8=" />
+        </div>
+
+        
       </div>
     </div>
   );
