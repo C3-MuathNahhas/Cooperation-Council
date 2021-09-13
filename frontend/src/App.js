1425964e174d57c6ffdd2cf8f0584024dbb19b6b
ahmadrokasha <ahmadrokasha@gmail.com>
2021-09-14 03:19:49 +0400

    app js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, createContext } from "react";
 import { Route, useRouteMatch, Switch, useHistory } from "react-router-dom";
-import { Home } from "./components/Home/Home";
+// import { Home } from "./components/Home/Home";
 import Table from "./components/ui/table";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
@@ -19,19 +19,16 @@ const App = () => {
   const state = { token, setToken };
 
   return (
-
-
     <div className="App">
-
       <Switch>
         <userContext.Provider value={state}>
           <Navigation />
           <Route exact path={`${path}/signUp`} component={SignUp} />
           <Route exact path={`${path}/login`} component={Login} />
-          <Route
+          {/* <Route
             path={`${path}/home`}
             render={() => <Home setvalue={setvalues} />}
-          />
+          /> */}
           {console.log(values)}
 
           <Route
