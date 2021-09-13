816c5b3f713e38aa7929cb9d36572ae8e3004ef3
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-14 00:01:51 +0300

    eneter id to test

--- frontend/src/components/updateFlightBooking/UpdateFlightBooking.js
+++ frontend/src/components/updateFlightBooking/UpdateFlightBooking.js
@@ -1,36 +1,35 @@
-import React, { useState} from "react";
-import { useHistory} from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 export const UpdateFlightBooking = () => {
- const [adult,setadult]=useState();
+  const [adult, setadult] = useState();
   const history = useHistory();
-  const update=(e)=>{
-
-  }
+  const update = (e) => {
+    setadult(e.target.value);
+  };
   const click = () => {
     axios
-      .put("http://localhost:5000/flightBooking/:bookingId,{
-        
+      .put("http://localhost:5000/flightBooking/613f7bbd7a7d0b4d7448f1b7", {
+        adult,
       })
       .then((result) => {
         //console.log(result)
-        
-        
-        
       });
   };
 
   return (
-      <>
-      
-    <div className="homed">
-     
-      <input type="number" placeholder="Enter the New Number of Adults" onChange={update}/>
-      <button type="button" onClick={click}>
-        Update
-      </button>
-    </div>
+    <>
+      <div className="homed">
+        <input
+          type="number"
+          placeholder="Enter the New Number of Adults"
+          onChange={update}
+        />
+        <button type="button" onClick={click}>
+          Update
+        </button>
+      </div>
     </>
   );
 };
