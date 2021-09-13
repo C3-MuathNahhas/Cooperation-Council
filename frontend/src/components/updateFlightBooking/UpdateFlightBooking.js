de32caaaa11517ecbdecd961b80c3085011c151b
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-13 23:59:27 +0300

    create onchnge event

--- frontend/src/components/updateFlightBooking/UpdateFlightBooking.js
+++ frontend/src/components/updateFlightBooking/UpdateFlightBooking.js
@@ -3,9 +3,11 @@ import { useHistory} from "react-router-dom";
 import axios from "axios";
 
 export const UpdateFlightBooking = () => {
- 
+ const [adult,setadult]=useState();
   const history = useHistory();
- 
+  const update=(e)=>{
+
+  }
   const click = () => {
     axios
       .put("http://localhost:5000/flightBooking/:bookingId,{
@@ -24,7 +26,7 @@ export const UpdateFlightBooking = () => {
       
     <div className="homed">
      
-      <input type="number"/>
+      <input type="number" placeholder="Enter the New Number of Adults" onChange={update}/>
       <button type="button" onClick={click}>
         Update
       </button>
