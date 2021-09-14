06f65d65299421e48a57e82e4e338ac5673fd991
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-14 01:10:33 -0700

    create header about

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -9,7 +9,10 @@ const About=()=>{
      <p>By investing in the technology that helps take the friction out of travel, Booking.com seamlessly connects millions of travellers with memorable experiences, a range of transport options and incredible places to stay - from homes to hotels and much more. As one of the world’s largest travel marketplaces for both established brands and entrepreneurs of all sizes, Booking.com enables properties all over the world to reach a global audience and grow their businesses.</p>
 
      <p>Booking.com is available in 43 languages and offers more than 28 million total reported accommodation listings, including over 6.2 million listings alone of homes, apartments and other unique places to stay. No matter where you want to go or what you want to do, Booking.com makes it easy and backs it all up with 24/7 customer support.</p>
-    
+     </div>
+     <div className="part_2">
+         <h2>What Traveleo.com offers</h2>
+     
     </div>
     
      </div>
