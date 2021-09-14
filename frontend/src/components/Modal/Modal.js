a19043c906a7304809e7b3473d5fc843c4897c34
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-14 07:14:39 -0700

    run button to push login

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -19,7 +19,16 @@ const Modal =()=>{
             <div className="modal-body">
             <p>We are happy to have you join us</p>
             </div>
-          
+            <div className="modal-footer">
+             <button onClick={()=>{
+            
+            // console.log(path)
+            // console.log("ahmed",url)
+             history.push(`/main/login`)
+            
+             }}   className="btn-cancel">Close</button>
+            
+            </div>
 
         </div>
         </div>
