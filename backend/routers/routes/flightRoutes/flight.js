b260ecfded7ddd227f02cdff47e37a7d34008c10
Mohammad ghazal <moh.g.ghazal@gmail.com>
2021-09-14 22:11:14 +0300

    fix the error

--- backend/routers/routes/flightRoutes/flight.js
+++ backend/routers/routes/flightRoutes/flight.js
@@ -2,18 +2,13 @@ const express = require("express");
 const {
   createNewFlight,
   getFlights,getAvailableFlights
-} = require("../controllers/flight/flightController");
-
-const { createNewFlight,getFlights}= require("../../controllers/flight/flightController");
+} = require("../../controllers/flight/flightController");
 const flightRouter = express.Router();
 
-flightRouter.post("/",createNewFlight)
+flightRouter.post("/", createNewFlight);
 
+flightRouter.post("/search", getFlights);
 
-flightRouter.post("/search",getFlights)
-
-
-flightRouter.get('/',getAvailableFlights)
+flightRouter.get("/", getAvailableFlights);
 
 module.exports = flightRouter;
- 
