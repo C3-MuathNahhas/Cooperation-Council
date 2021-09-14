05e6763d29f5fbfdf8aad4cc3ab68552b644d081
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-15 00:33:28 +0300

    update files

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -3,39 +3,38 @@ import { useHistory, useRouteMatch } from "react-router-dom";
 import axios from "axios";
 import "../Home/Home.css";
 export const Home = ({ setvalue }) => {
-  const destenations = ["San Francisco", "Amman", "Tripoli", "Plockton", "Carthage"];
+  const destenations = [
+    "San Francisco",
+    "Amman",
+    "Tripoli",
+    "Plockton",
+    "Carthage",
+  ];
   const origins = ["San Francisco", "Amman", "Tripoli", "Plockton", "Carthage"];
 
-  let flights2 = [];
-  let des = [];
-
- 
-  
   let { path, url } = useRouteMatch();
   const [origin, setOrigin] = useState();
   const [destination, setDestination] = useState();
-  const [date, setdate] = useState();
+  const [dateFrom, setDateFrom] = useState();
+  const [adults, setadults] = useState();
+  const [dateTo, , setDateTo] = useState();
   const history = useHistory();
 
-  const dd = (f) => {
-    setdate(f.target.value);
-  };
   const click = () => {
     axios
-      .post("http://localhost:5000/flights/search", {
+      .post("http://localhost:5000/flights/", {
         origin,
         destination,
-        date,
+        adults,
+        dateFrom,
+        dateTo,
       })
       .then((result) => {
-        console.log(result)
+        console.log(result);
         setvalue(result.data);
-        history.push(`${path} / Table`)
-
+        history.push(`${path} / Table`);
       });
   };
-
-
 
   return (
     <>
@@ -44,33 +43,60 @@ export const Home = ({ setvalue }) => {
         <div className="bigContainer">
           <div id="container">
             <h1>origin</h1>
-            <select id="origin" name="origin" onChange={(r) => { setOrigin(r.target.value) }} required>
-              {
-                destenations.map((item) => {
-                  return <option value={item}>{item}</option>
-                })
-              }
-
+            <select
+              id="origin"
+              name="origin"
+              onChange={(r) => {
+                setOrigin(r.target.value);
+              }}
+              required
+            >
+              {destenations.map((item) => {
+                return <option value={item}>{item}</option>;
+              })}
             </select>
           </div>
           <div id="container">
             <h1>destination</h1>
-            <select id="destination" name="destination" onChange={(r) => { setDestination(r.target.value) }} required>
-              {
-                origins.map((item) => {
-                  return <option value={item}>{item}</option>
-                })
-              }
-
+            <select
+              id="destination"
+              name="destination"
+              onChange={(r) => {
+                setDestination(r.target.value);
+              }}
+              required
+            >
+              {origins.map((item) => {
+                return <option value={item}>{item}</option>;
+              })}
             </select>
           </div>
         </div>
-
+        <h1>adults</h1>
+        <input
+          className="dateInput"
+          type="number"
+          onChange={(w) => {
+            setadults(w.target.value);
+          }}
+        />
         <h1>Date</h1>
         <label>from</label>
-        <input className='dateInput' type="date" onChange={dd} />
+        <input
+          className="dateInput"
+          type="date"
+          onChange={(e) => {
+            setDateFrom(e.target.value);
+          }}
+        />
         <label>to</label>
-        <input className='dateInput' type="date" onChange={dd} />
+        <input
+          className="dateInput"
+          type="date"
+          onChange={(r) => {
+            setDateTo(r.target.value);
+          }}
+        />
 
         <button type="button" onClick={click}>
           Search
