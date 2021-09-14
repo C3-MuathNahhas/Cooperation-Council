5e2114987a99d0e64e4d39ff0040238200fe47d5
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-14 08:22:43 -0700

    updated

--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -1,88 +1,95 @@
-import React,{useState} from "react";
+import React, {useState } from "react";
 import axios from "axios";
+import "../auth/SignUp.css";
+import Modal from "../Modal/Modal";
 
-const SignUp =()=>{
-  const [firstName, setFirstName] =useState();
-  const [lastName, setLastName] =useState();
-  const [phoneNumber, setPhoneNumber] =useState();
-  const [email, setEmail] =useState();
-  const [password, setPassword] =useState();
-  const [status, setStatus] =useState();
+const SignUp = () => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [status, setStatus] = useState();
+  const [show, setShow] = useState(false);
 
   return (
-    <div className="signUp">
-        
-      <input
-        type="text"
-        placeholder="firstName"
-        onChange={(e) => {
-          setFirstName(e.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="lastName"
-        onChange={(e) => {
-          setLastName(e.target.value);
-        }}
-      />
+    <>
+      {!show ? (
+        <div className="signUp">
+          <input
+            type="text"
+            placeholder="firstName"
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="lastName"
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
 
-      <input
-        type="number"
-        placeholder="phoneNumber"
-        onChange={(e) => {
-          setPhoneNumber(e.target.value);
-        }}
-      />
-      <input
-        type="email"
-        placeholder="email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          axios
-            .post("http://localhost:5000/signUp",{
-              firstName,
-              lastName,
-              phoneNumber,
-              email,
-              password,
-            })
-            .then((result) => {
-          //    console.log(result)
-              setStatus(
-                <div className="success">
-                  the user has been added successfully
-                </div>
-              );
-            })
-            .catch((err) => {
-              setStatus(
-                <div className="wrong">
-                  error happen while regeister please try again
-                </div>
-              );
-            });
-        }}
-      >
-        signUp
-      </button>
-      {status}
-      
+          <input
+            type="number"
+            placeholder="phoneNumber"
+            onChange={(e) => {
+              setPhoneNumber(e.target.value);
+            }}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              axios
+                .post("http://localhost:5000/signUp", {
+                  firstName,
+                  lastName,
+                  phoneNumber,
+                  email,
+                  password,
+                })
+                .then((result) => {
+                  setShow(true);
 
-    </div>
-  )
-}
+                  //    console.log(result)
+                  setStatus(
+                    <div className="success">
+                      the user has been added successfully
+                    </div>
+                  );
+                })
+                .catch((err) => {
+                  setStatus(
+                    <div className="wrong">
+                      error happen while regeister please try again
+                    </div>
+                  );
+                });
+            }}
+          >
+            signUp
+          </button>
+          {status}
+        </div>
+      ) : (
+        <Modal  state={email,password}/>
+      )}
+    </>
+  );
+};
 
-
-export default SignUp
+export default SignUp;
