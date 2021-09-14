f24b8b43edca5511ff55bb43bbf677d0026e1111
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-14 16:40:31 +0300

    marge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,42 +12,43 @@ import Cities from "../src/components/ui/cities";
 import Footer from "../src/components/footer/footer";
 import { UpdateFlightBooking } from "./components/updateFlightBooking/UpdateFlightBooking";
 
-import Contact from './components/Contact';
-
-
+import Contact from "./components/contact/Contact";
+export const userContext=createContext();
 const App = () => {
-	const [values, setvalues] = useState();
-	let { path, url } = useRouteMatch();
+	
+  const [values, setvalues] = useState();
+  let { path, url } = useRouteMatch();
   const [token, setToken] = useState();
   const state = { token, setToken };
 
   return (
     <div className="App">
-		<Route
-            path={`${path}/Update`}
-            render={() => <UpdateFlightBooking value={values} state={state} />}
-          />
+      <Route
+        path={`${path}/Update`}
+        render={() => <UpdateFlightBooking value={values} state={state} />}
+      />
       <Switch>
         <userContext.Provider value={state}>
           <Navigation />
 
-	<Route path={`${path}/signUp`}component={SignUp} />
-	<Route path= {`${path}/login`}component={Login}/>
-	<Route path={`${path}/contact`}component={Contact} />
-  <Route path={`${path}/home`}
+          <Route path={`${path}/signUp`} component={SignUp} />
+          <Route path={`${path}/login`} component={Login} />
+          <Route path={`${path}/contact`} component={Contact} />
+          <Route
+            path={`${path}/home`}
             render={() => <Home setvalue={setvalues} />}
           />
-<Route path={`${path}/Table`}
+          <Route
+            path={`${path}/Table`}
             render={() => <Table value={values} state={state} />}
           />
-	</userContext.Provider>
-
+        </userContext.Provider>
+      </Switch>
       <Route path={`${path}/mainPage`} component={Weather}></Route>
       <Route path={`${path}/mainPage`} component={Cities}></Route>
       <Route path={`${path}/mainPage`} component={Attractions}></Route>
       <Route path={`${path}/mainPage`} component={Footer}></Route>
     </div>
-
   );
 };
 
