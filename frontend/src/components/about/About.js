61704eb005f1dbbaaf5c4bca309407572161729e
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-14 01:08:47 -0700

    create header about

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -3,7 +3,11 @@ import "../about/About.css"
 const About=()=>{
  return(
      <div className="About">
-         
+         <div className="part_1">
+     <h1>About Traveleo.com™</h1>
+    
+    </div>
+    
      </div>
  )
 }
