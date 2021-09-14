f0bb2106fcabf44e0e9d14ed66d3b59aa4102460
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-14 01:09:31 -0700

    create paragraph

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -5,7 +5,8 @@ const About=()=>{
      <div className="About">
          <div className="part_1">
      <h1>About Traveleo.com™</h1>
-    
+     <p>Founded in 2021 in Jordan, Traveleo.com has grown from a small Dutch startup to one of the world’s leading digital travel companies. Part of Booking Holdings Inc. (NASDAQ: BKNG), Booking.com’s mission is to make it easier for everyone to experience the world.</p>
+  
     </div>
     
      </div>
