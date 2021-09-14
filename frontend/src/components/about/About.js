49905c87f25b30fbdd1c1ac971aa713f12b4fea9
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-14 01:09:55 -0700

    create paragraph

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -6,7 +6,10 @@ const About=()=>{
          <div className="part_1">
      <h1>About Traveleo.com™</h1>
      <p>Founded in 2021 in Jordan, Traveleo.com has grown from a small Dutch startup to one of the world’s leading digital travel companies. Part of Booking Holdings Inc. (NASDAQ: BKNG), Booking.com’s mission is to make it easier for everyone to experience the world.</p>
-  
+     <p>By investing in the technology that helps take the friction out of travel, Booking.com seamlessly connects millions of travellers with memorable experiences, a range of transport options and incredible places to stay - from homes to hotels and much more. As one of the world’s largest travel marketplaces for both established brands and entrepreneurs of all sizes, Booking.com enables properties all over the world to reach a global audience and grow their businesses.</p>
+
+     <p>Booking.com is available in 43 languages and offers more than 28 million total reported accommodation listings, including over 6.2 million listings alone of homes, apartments and other unique places to stay. No matter where you want to go or what you want to do, Booking.com makes it easy and backs it all up with 24/7 customer support.</p>
+    
     </div>
     
      </div>
