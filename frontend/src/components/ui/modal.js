37b03954f473eb3434f8133e0eebeb3a881df54d
ahmadrokasha <ahmadrokasha@gmail.com>
2021-09-16 02:43:07 +0400

    modal  feature

--- frontend/src/components/ui/modal.js
+++ frontend/src/components/ui/modal.js
@@ -10,7 +10,12 @@ function Modal(props) {
       {props.src && (
         <div className="modal_img_div">
           <img src={props.src} className="modal_img"></img>
-          <div className="modal_img_details">hi</div>
+          <iframe
+            className="modal_img_details"
+            src="https://en.wikipedia.org/wiki/Paris"
+          >
+            hi
+          </iframe>
         </div>
       )}
       {props.src && <div className="back_div" onClick={hideBack}></div>}
