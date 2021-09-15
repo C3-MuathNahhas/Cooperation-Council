221184fcaff7604fa020133ad434a044062e54f6
ahmadrokasha <ahmadrokasha@gmail.com>
2021-09-15 22:16:57 +0400

    modal  feature

--- frontend/src/components/ui/modal.js
+++ frontend/src/components/ui/modal.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './modal.css'
-
-
+import React from "react";
+import "./modal.css";
 
 function Modal(props) {
-    return (
-        <div className='modal_div'>
-           <img className='modal_img' src={props.src}></img> 
-        </div>
-    )
+  return (
+    <div className='modal_div'>
+      <div className="img_div">
+        {props.src && <img src={props.src}></img>}
+          </div>
+          <div className='back_div'></div>
+    </div>
+  );
 }
 
-export default Modal
+export default Modal;
