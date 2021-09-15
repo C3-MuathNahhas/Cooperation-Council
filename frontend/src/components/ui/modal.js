6ddd71a08ff3621e77131d04d0764ddad9de369d
ahmadrokasha <ahmadrokasha@gmail.com>
2021-09-15 22:35:41 +0400

    modal  feature

--- frontend/src/components/ui/modal.js
+++ frontend/src/components/ui/modal.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./modal.css";
 
 function Modal(props) {
+  const hideBack = () => {
+    props.hideBack("");
+  };
   return (
     <div className="modal_div">
       {props.src && (
