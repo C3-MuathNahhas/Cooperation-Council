29591dea1938c459672c6149df49d7a8a2278749
ahmadrokasha <ahmadrokasha@gmail.com>
2021-09-15 16:09:59 +0400

    modal  feature

---
+++ frontend/src/components/ui/modal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './modal.css'
+
+function modal() {
+    return (
+        <div className='modal_div'>
+            
+        </div>
+    )
+}
+
+export default modal
