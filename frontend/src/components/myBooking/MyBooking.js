80fd9562e7c3d59233f60e272aaff706e10cdcbd
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-17 02:37:43 +0300

    work on myBooking

---
+++ frontend/src/components/myBooking/MyBooking.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
+import axios from "axios";
+import "./Home.css";
+
+export const MyBooking = ({ setvalue, setadult }) => {
+  const click = () => {};
+
+  return <div></div>;
+};
