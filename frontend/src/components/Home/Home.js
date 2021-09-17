b9bb8592bd3c919bf14195718a373ccb76f09290
Mohammad ghazal <moh.g.ghazal@gmail.com>
2021-09-17 04:07:44 +0300

    name edit

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -77,7 +77,7 @@ export const Home = ({ setvalue, setadult }) => {
     <div className="homeBody">
       <div className="smallBody">
         <h1 style={{ fontSize: "20px" }}>
-          <span style={{ color: "rgb(19,145,210)" }}>tre</span>
+          <span style={{ color: "rgb(19,145,210)" }}>Tra</span>
           <span style={{ color: "rgb(252,158,21)" }}>val</span>
           <GiPlanePilot style={{ fontSize: "3vw" }} />
           <span style={{ color: "rgb(227,64,61)" }}>eo</span>
