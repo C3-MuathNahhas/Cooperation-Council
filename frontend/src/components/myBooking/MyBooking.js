fe64c822dd1511d9850470f5e4bf0555db8b7eff
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-17 21:25:29 +0300

    update photo

--- frontend/src/components/myBooking/MyBooking.js
+++ frontend/src/components/myBooking/MyBooking.js
@@ -3,47 +3,44 @@ import { useHistory, useRouteMatch } from "react-router-dom";
 import axios from "axios";
 import "./MyBooking.css";
 
-export const MyBooking = ({book,adult}) => {
+export const MyBooking = ({ book, adult }) => {
   const [value, setvalue] = useState();
   const click = () => {};
-  console.log("book",book)
+  console.log("book", book);
   return (
     <div>
-      {book&&book.map((element)=>{return(<div>
+      {book &&
+        book.map((element) => {
+          return (
+            <div>
+              <div className="card">
+                <div className="image"></div>
+                <div className="text">
+                  <span className="date">Your Book</span>
+                  <h2 className="date">Date: {element.date}</h2>
 
-
-        <div className="card">
-        <div className="image"></div>
-        <div className="text">
-          <span className="date">Your Book</span>
-          <p className="date">{element.date}</p>
-      
-          <h2>{element.destination}</h2>
-          <button className="btn1">Update</button>
-          <button className="btn1">Delete</button>
-        </div>
-        <div className="status">
-          <div className="stat">
-            <div className="value">Capacity</div>
-            <div className="type">{element.capacity}</div>
-          </div>
-          <div className="stat">
-            <div className="value">TotalPrice</div>
-            <div class="type">{element.totalPrice}$</div>
-          </div>
-          <div className="stat">
-            <div className="value">Adults</div>
-            <div class="type">{adult}</div>
-          </div>
-        </div>
-      </div>
-
-
-
-
-
-      </div>)})}
-      
+                  <h2>Destination: {element.destination}</h2>
+                  <button className="btn1">Update</button>
+                  <button className="btn1">Delete</button>
+                </div>
+                <div className="status">
+                  <div className="stat">
+                    <div className="value">Capacity</div>
+                    <div className="type">{element.capacity}</div>
+                  </div>
+                  <div className="stat">
+                    <div className="value">TotalPrice</div>
+                    <div class="type">{element.totalPrice}$</div>
+                  </div>
+                  <div className="stat">
+                    <div className="value">Adults</div>
+                    <div class="type">{adult}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
