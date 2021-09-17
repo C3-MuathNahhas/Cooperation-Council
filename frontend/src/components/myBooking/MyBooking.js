0ad852733cf9d9735ca63329ec1a73783d186661
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-17 20:53:22 +0300

    add data

--- frontend/src/components/myBooking/MyBooking.js
+++ frontend/src/components/myBooking/MyBooking.js
@@ -9,22 +9,25 @@ export const MyBooking = ({book}) => {
   console.log("book",book)
   return (
     <div>
-      <div className="card">
+      {book&&book.map((element)=>{return(<div>
+
+
+        <div className="card">
         <div className="image"></div>
         <div className="text">
           <span className="date">Your Book</span>
-          <h2>Welcome</h2>
+          <h2>{element.destination}</h2>
           <button className="btn1">Update</button>
           <button className="btn1">Delete</button>
         </div>
         <div className="status">
           <div className="stat">
-            <div className="value">capacity</div>
-            <div className="type">Capacity</div>
+            <div className="value">Capacity</div>
+            <div className="type">{element.capacity}</div>
           </div>
           <div className="statborder">
-            <div className="value"></div>
-            <div class="type">views</div>
+            <div className="value">TotalPrice</div>
+            <div class="type">{element.totalPrice}</div>
           </div>
           <div className="stat">
             <div className="value">32</div>
@@ -32,6 +35,13 @@ export const MyBooking = ({book}) => {
           </div>
         </div>
       </div>
+
+
+
+
+
+      </div>)})}
+      
     </div>
   );
 };
