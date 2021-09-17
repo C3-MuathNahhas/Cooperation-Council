1a6a0dd1d0ddd4d902aabb91936c3f0c9f021009
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-17 19:46:14 +0300

    status

--- frontend/src/components/myBooking/MyBooking.js
+++ frontend/src/components/myBooking/MyBooking.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import { useHistory, useRouteMatch } from "react-router-dom";
 import axios from "axios";
-import "./MyBooking.css"
+import "./MyBooking.css";
 
 export const MyBooking = () => {
-    const[value,setvalue]=useState()
-  const click = () => {
-      
-  };
+  const [value, setvalue] = useState();
+  const click = () => {};
 
   return (
     <div>
-<div className="card"></div>
-<div className="text"></div>
+      <div className="card">
+        <div className="image"></div>
+        <div className="text">
+          <span className="date">??</span>
+        </div>
+        <div className="status"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
