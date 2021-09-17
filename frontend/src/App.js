3de0f1ab272f9815fde53ace57a8bb27f86752d8
ahmadrokasha <ahmadrokasha@gmail.com>
2021-09-18 01:27:49 +0400

    app.js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, createContext } from "react";
-import { Home } from "./components/home/Home";
+import Home from "./components/Home/Home";
 import Table from "./components/ui/table";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
+import logOut from "./components/logout/logOut";
+
 import Navigation from "../src/components/navigation/Navigation";
 import About from "./components/about/About";
 import "./App.css";
@@ -33,6 +35,7 @@ const App = () => {
         <Switch>
           <userContext.Provider value={state}>
             <Navigation />
+
             <div className="navPlus" />
             <Route
               path={`${path}/Update`}
@@ -45,6 +48,8 @@ const App = () => {
             <userSign.Provider value={{email, password,setEmail,setPassword}}>
               <Route path={`${path}/signUp`} component={SignUp} />
               <Route path={`${path}/login`} component={Login} />
+              <Route path={`${path}/logout` }component={logOut}/>
+
             </userSign.Provider>
 
             <Route path={`${path}/contact`} component={Contact} />
@@ -57,13 +62,15 @@ const App = () => {
               path={`${path}/Table`}
               render={() => <Table value={values} state={state} adult={adult} />}
             />
+            {/* <Route exact path={path} component={Weather}></Route> */}
+        <Route exact path={path} component={Cities}></Route>
+        <Route exact path={path} component={Attractions}></Route>
+        <Route exact path={path} component={SideBar}></Route>
           </userContext.Provider>
         </Switch>
-        <Route path={`${path}/mainPage`} component={Weather}></Route>
-        <Route path={`${path}/mainPage`} component={Cities}></Route>
-        <Route path={`${path}/mainPage`} component={Attractions}></Route>
-        <Route path={`${path}/mainPage`} component={Footer}></Route>
-        <Route path={`${path}/mainPage`} component={SideBar}></Route>
+        
+        <Footer/>
+
       </div>
     </div>
   );
